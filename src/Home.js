4476aa192de91e8ecfdfd45b898b4dcb1a9c18c1
Arthur Barros <40128694+Artmoraes@users.noreply.github.com>
2022-08-23 00:19:54 -0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,16 @@ import './App.css';
 
 function Home() {
   return (
-    <div className="App space--button">
+    <div className="App">
       <h1>Home</h1>
+    <div className="space--button">
       <Link to='/personality'>
         <button type="button" name='Personality'>Personality</button>
       </Link>
       <Link to='/formal'>
         <button type="button" name='Formal'>Formal</button>
-      </Link>
+      </Link>    
+    </div>
     </div>
   );
 }
