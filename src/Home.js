d1f00959e24a44758ca29cd9c1e560eda5973ba5
Arthur Barros <Artmorraes1@gmail.com>
2022-08-23 01:02:48 -0300

    Concluindo a tela home portfolio

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,31 @@
 import { Link } from 'react-router-dom';
 import './App.css';
+import './Home.css'
 
 function Home() {
   return (
-    <div className="App">
+    <div className="App bodyColor">
       <h1>Home</h1>
-    <div className="space--button">
-      <Link to='/personality'>
-        <button type="button" name='Personality'>Personality</button>
-      </Link>
-      <Link to='/formal'>
-        <button type="button" name='Formal'>Formal</button>
-      </Link>    
-    </div>
+      <div className="space--button">
+        <section>
+          <Link to='/personality'>
+            <button type="button" name='Personality' className='button--personality'>
+              <span>
+                Personality
+              </span>
+            </button>
+          </Link>
+        </section>
+        <section>
+          <Link to='/formal'>
+            <button type="button" name='Formal'  className='button--formal'>
+              <span class="shadow"></span>
+              <span class="edge"></span>
+              <span class="front text">Formal</span>            
+            </button>
+          </Link>    
+        </section>
+      </div>
     </div>
   );
 }
